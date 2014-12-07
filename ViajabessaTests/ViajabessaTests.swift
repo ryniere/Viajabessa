86771f7940e5cf0c40fa1b0b795569f9f9235fe4
ryniere <Ryniere@Rynieres-MacBook-Pro-2.local>
2014-12-07 15:00:38 -0200

    Commit inicial

---
+++ ViajabessaTests/ViajabessaTests.swift
@@ -0,0 +1,36 @@
+//
+//  ViajabessaTests.swift
+//  ViajabessaTests
+//
+//  Created by Ryniere dos Santos Silva on 07/12/14.
+//  Copyright (c) 2014 Ryniere dos Santos Silva. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class ViajabessaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
